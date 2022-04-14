24e10ff1497ab0c2e0de73c8d93a68e4f7d0b988
Anthony <anthony.claudon21170@gmail.com>
2022-04-14 14:18:35 +0200

    Initial commit

---
+++ lib/00_journalists.rb
@@ -0,0 +1,105 @@
+journalists = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+def premier(journalists)
+    puts "########################################################################"
+    puts "Exercice n°1 : Combien y'a t-il de handles dans ce tableau ?"
+    print "\n"
+    puts "Nombre de handles = #{journalists.count}"
+    print "\n"
+    puts "Pour continuer appuyer sur ENTREE"
+    puts "########################################################################"
+    gets.chomp
+end
+
+def deuxieme(journalists)
+    puts "########################################################################"
+    puts "Exercice n°2 : Handle le plus court ?"
+    print "\n"
+    puts "Handle le plus court : " + journalists.min {|a, b| a.size <=> b.size}
+    print "\n"
+    puts "Pour continuer appuyer sur ENTREE"
+    puts "########################################################################"
+    gets.chomp
+end
+
+def troisieme(journalists)
+    puts "########################################################################"
+    puts "Exercice n°3 : Combien y'a t-il de handles avec 5 caractères dans ce tableau ?"
+    five_char_username = journalists.count {|five_char| five_char.size == 6}
+    puts five_char_username
+    puts "Pour continuer appuyer sur ENTREE"
+    puts "########################################################################"
+    gets.chomp
+end
+
+def quatrieme(journalists)
+    puts "########################################################################"
+    puts "Exercice n°4 : Combien y'a t-il de handles qui commencent par une majuscule dans ce tableau ?"
+    puts "Il y a #{journalists.count {|e| e =~ /^@[A-Z]/}} qui commencent par une majuscule"
+    puts "Pour continuer appuyer sur ENTREE"
+    puts "########################################################################"
+    gets.chomp
+end
+
+def cinquieme(journalists)
+    puts "########################################################################"
+    puts "Exercice n°5 : Trier le tableau par ordre alphabétique des handles"
+    liste = journalists.sort_by(&:upcase)
+    puts liste
+    puts "Pour continuer appuyer sur ENTREE"
+    puts "########################################################################"
+    gets.chomp
+end
+
+def sixieme(journalists)
+    puts "########################################################################"
+    puts "Exercice n°6 : Trier le tableau par ordre de taille des handles"
+    liste = journalists.sort_by(&:length)
+    puts liste
+    puts "Pour continuer appuyer sur ENTREE"
+    puts "########################################################################"
+    gets.chomp
+end
+
+def septieme(liste)
+    puts "########################################################################"
+    puts "Exercice n°7 : À quelle place dans le tableau ce trouve le compte @epenser ?"
+    puts liste.find_index("@epenser")
+    puts "Pour continuer appuyer sur ENTREE"
+    puts "########################################################################"
+    gets.chomp
+end
+
+def huitieme(journalists)
+    puts "########################################################################"
+    puts "Exercice n°8 : Répartir par taille les handles avec 1, 2 caractères ou plus"
+    puts "Les twitter handle triés par nombre de caractères :"
+    print "\n"
+    y = 2
+    i = 33
+    a = 1
+    while y <= i
+        if (journalists.count {|x| x.size == y}) !=0
+        puts "Il y a #{journalists.count {|x| x.size == y}} handle(s) avec #{a} caractère(s)."
+        end
+        y += 1
+        a += 1
+    end
+    print "\n"
+    puts "Appuyez pour quitter !"
+    puts "########################################################################"
+    gets.chomp
+end
+
+def perform(journalists)
+    premier(journalists)
+    deuxieme(journalists)
+    troisieme(journalists)
+    quatrieme(journalists)
+    cinquieme(journalists)
+    sixieme(journalists)
+    septieme(journalists)
+    huitieme(journalists)
+end
+
+perform(journalists)
